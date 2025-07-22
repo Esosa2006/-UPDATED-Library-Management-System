a9589c8f59d3d60d39f37a1bcc39f40d812b1700
Esosa Uwuilekhue <esosauwuilekhue@gmail.com>
2025-07-22 15:25:23 +0100

    Refactor: Changed Exception class to record

--- src/main/java/com/LBS/Library/Management/System/exceptions/Exception.java
+++ src/main/java/com/LBS/Library/Management/System/exceptions/Exception.java
@@ -1,20 +1,9 @@
 package com.LBS.Library.Management.System.exceptions;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-@Getter
-public class Exception {
-    private final String message;
-    private final HttpStatus httpStatus;
-    private final ZonedDateTime timestamp;
-
-    public Exception(String message, HttpStatus httpStatus, ZonedDateTime timestamp) {
-        this.message = message;
-        this.httpStatus = httpStatus;
-        this.timestamp = timestamp;
-    }
+public record Exception (String message, HttpStatus httpStatus, ZonedDateTime timestamp){
 
 }
